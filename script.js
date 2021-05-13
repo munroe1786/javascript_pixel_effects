607e3e8cf837238b71c0d05afbc1cd9a19cf3e0a
munroe1786 <era3munroe@gmail.com>
2021-05-13 07:57:32 -0500

    build out image1 event listener in script.js

--- script.js
+++ script.js
@@ -7,5 +7,7 @@ const image1 = new Image();
 image1.src = 'image1.png';
 
 image1.addEventListener('load', function(){
-    ctx.drawImage(image, 0, 0);
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    const scannedImage = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    console.log(scannedImage);
 })
